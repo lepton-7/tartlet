a38bd4f12d7f7a3d7ee0f8bc4e2068d5e7978825
lepton-7 <sachitkshatriya2609@gmail.com>
2023-10-04 19:02:05 -0400

    Installed bio-tart

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
 setup(
-    name="TaRT",
+    name="bio-tart",
     version="0.1.0",
     author="Sachit Kshatriya",
     author_email="sxk1464@case.edu",
-    license="<the license you chose>",
+    license="MIT",
     description="Trace riboswitches modulating transcription-termination.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lepton-7/tart",
-    py_modules=["my_tool", "app"],
+    py_modules=["targeted_pipeline"],
     packages=find_packages(),
     install_requires=[requirements],
     python_requires=">=3.9,<3.12",

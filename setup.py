768b783bfcd886dac1331a7d53e5dc9e35b49615
lepton-7 <sachitkshatriya2609@gmail.com>
2023-10-04 23:41:03 -0400

    Changed sub command names

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="bio-tart",
-    version="0.1.0",
+    version="0.1.1",
     author="Sachit Kshatriya",
     author_email="sxk1464@case.edu",
     license="MIT",

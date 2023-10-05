0f8b5862daa4162fb7713f095e334cb9d3aeffb5
lepton-7 <sachitkshatriya2609@gmail.com>
2023-10-05 15:22:55 -0400

    Data subdir path handler

--- tartlet/utils/utils.py
+++ tartlet/utils/utils.py
@@ -1,4 +1,6 @@
 import click
+import importlib_resources
+import pathlib
 
 
 def print(obj):
@@ -8,3 +10,14 @@ def print(obj):
         obj (Any): Object to print
     """
     click.echo(obj)
+
+
+def get_datapath_obj() -> pathlib.Path:
+    """Get a path for the data subdirectory independent of package installation knowledge.
+
+    Returns:
+        pathlib.Path: Data subdirectory
+    """
+    utils = importlib_resources.files("tart.utils")
+    with importlib_resources.as_file(utils / "data") as path:
+        return path

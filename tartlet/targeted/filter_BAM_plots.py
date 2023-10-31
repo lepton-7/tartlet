cffa87a2d186cd36e9c3dab3e9f157eebd55cef3
lepton-7 <sachitkshatriya2609@gmail.com>
2023-10-31 11:35:30 -0400

    Added candidate summit to log

--- tartlet/targeted/filter_BAM_plots.py
+++ tartlet/targeted/filter_BAM_plots.py
@@ -14,8 +14,9 @@ from tart.utils.activity_inference import has_candidate_peak
 
 
 def _log_cand_charac(align_charac: dict, cand: Candidate):
-    align_charac["peak_width"] = cand.width
     align_charac["coverage_delta"] = cand.abs_cov_delta
+    align_charac["peak_width"] = cand.width
+    align_charac["peak_summit"] = cand.summit
     align_charac["coverage_delta_relative"] = cand.rel_cov_delta
     align_charac["coverage_delta_noiseset"] = str(cand.coverage_delta_noise)
 

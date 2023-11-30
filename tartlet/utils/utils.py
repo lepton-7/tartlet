1fc88661ab2936b8e13e3e8085b9744f3129886c
lepton-7 <sachitkshatriya2609@gmail.com>
2023-11-30 12:24:23 -0500

    Rowid generating function

--- tartlet/utils/utils.py
+++ tartlet/utils/utils.py
@@ -1,6 +1,8 @@
 import click
-import importlib_resources
 import pathlib
+import importlib_resources
+
+from pandas import Series
 
 
 def print(obj):
@@ -21,3 +23,24 @@ def get_datapath_obj() -> pathlib.Path:
     utils = importlib_resources.files("tart.utils")
     with importlib_resources.as_file(utils / "data") as path:
         return path
+
+
+def rowid(row: Series) -> str:
+    """Generate the rowid for a ledger row.
+
+    This should be used across the entire package when determining the rowids.
+
+    Args:
+        row (Series): Row element for which to generate the ID
+
+    Returns:
+        str: rowid
+    """
+    classname = row["target_name"]
+    qname = row["query_name"]
+    frm = row["seq_from"]
+    to = row["seq_to"]
+    strand = row["strand"]
+
+    # No spaces to ensure the entire string is recognised as the ID
+    return f"{classname}#{qname}#{frm}#{to}#{strand}"

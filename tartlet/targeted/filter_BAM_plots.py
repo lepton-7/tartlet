adb04f3d94eb92b3543341a6ab79126443e4dc58
lepton-7 <sachitkshatriya2609@gmail.com>
2024-02-08 01:51:01 -0500

    Rework to output all peak info within the region of interest

--- tartlet/targeted/filter_BAM_plots.py
+++ tartlet/targeted/filter_BAM_plots.py
@@ -10,24 +10,60 @@ from tart.utils.read_parsing import AlignDat, SegregatedAlignDat
 from tart.utils.mpi_context import BasicMPIContext
 from tart.utils.activity_inference import Candidate
 from tart.utils.filter_functions import DefaultThresholds as checker
-from tart.utils.activity_inference import has_candidate_peak
+from tart.utils.activity_inference import has_candidate_peak, get_peaks
 
 
-def _log_cand_charac(align_charac: dict, cand: Candidate):
-    align_charac["coverage_delta"] = cand.abs_cov_delta
-    align_charac["coverage_delta_pval"] = cand.coverage_drop_pvalue
-    align_charac["peak_summit"] = cand.summit
-    align_charac["peak_width"] = cand.width
-    # align_charac["peak_l/r"] = (cand.left, cand.right)
-    align_charac["coverage_delta_relative"] = cand.rel_cov_delta
-    align_charac["coverage_delta_noiseset"] = str(cand.coverage_delta_noise)
+def _log_cand_charac(peaklog: dict, cand: Candidate):
+    peaklog["coverage_delta"] = cand.abs_cov_delta
+    peaklog["coverage_delta_pval"] = cand.coverage_drop_pvalue
+    peaklog["peak_summit"] = cand.summit
+    peaklog["peak_width"] = cand.width
+    # peaklog["peak_l/r"] = (cand.left, cand.right)
+    peaklog["coverage_delta_relative"] = cand.rel_cov_delta
+    peaklog["coverage_delta_noiseset"] = str(cand.coverage_delta_noise)
 
-    align_charac["from_riboswith_end"] = cand.from_switch_end
-    align_charac["from_riboswith_end_relative"] = cand.from_switch_end_relative
+    peaklog["from_riboswith_end"] = cand.from_switch_end
+    peaklog["from_riboswith_end_relative"] = cand.from_switch_end_relative
 
-    align_charac["ks_stat"] = cand.symks_stat
-    align_charac["ks_p"] = cand.symks_pval
-    align_charac["decision_note"] = cand.note
+    # peaklog["ks_stat"] = cand.symks_stat
+    # peaklog["ks_p"] = cand.symks_pval
+    peaklog["decision_note"] = cand.note
+
+
+def _process_peak(peak: Candidate, peaklog: dict, peaklog_loc: list):
+
+    dec = checker.check(peak)
+    peaklog["decision"] = dec
+
+    _log_cand_charac(peaklog, peak)
+
+    peaklog_loc.append(peaklog)
+
+    return True if dec == "pass" else False
+
+
+def _load_alignment_data(pick_root: str, pick_file: str, rank: int):
+    """Helper to minimise nesting.
+
+    Args:
+        pick_root (str): Archive root.
+        pick_file (str): Pickle filename to obtain from the archive.
+        rank (int): MPI worker rank.
+
+    Returns:
+        segmented: SegregatedAlignDat object.
+    """
+    with tarfile.open(pick_root, "r:gz") as picktar:
+        try:
+            with picktar.extractfile(pick_file) as f:  # type: ignore
+                segmented: SegregatedAlignDat = pickle.load(f)
+
+                return segmented
+
+        except KeyError:
+            print(f"{pick_file} not found in archive {pick_root} on worker rank {rank}")
+
+            return None
 
 
 def _process_candidate_list(
@@ -37,6 +73,18 @@ def _process_candidate_list(
     ref: str,
     transcriptome: str,
 ):
+    """DEPR
+
+    Args:
+        candlist (list[Candidate]): _description_
+        align_charac (dict): _description_
+        charac_local (list): _description_
+        ref (str): _description_
+        transcriptome (str): _description_
+
+    Returns:
+        _type_: _description_
+    """
     if len(candlist) == 0:
         align_charac["decision_note"] = "No suitable candidates"
         align_charac["decision"] = "fail"
@@ -126,13 +174,141 @@ def exec_main(
     statplot,
 ):
     if run_depr:
-        raise ValueError("No deprecated function to run")
+        depr_main(pick_root, out_dir, bin_size, min_cov_depth, ext_prop, conv, statplot)
+        # raise ValueError("No deprecated function to run")
 
     else:
         main(pick_root, out_dir, bin_size, min_cov_depth, ext_prop, conv, statplot)
 
 
 def main(pick_root: str, out_dir, bin_size, min_cov_depth, ext_prop, conv, statplot):
+    # Determine MPI context ---------------------------------------------------
+    mp_con = BasicMPIContext()
+    comm = mp_con.comm
+    rank = mp_con.rank
+
+    # List of all pickled alignment data
+    if rank == 0:
+        try:
+            with tarfile.open(pick_root, "r:gz") as picktar:
+                total_files = [x.name for x in picktar.getmembers() if x.isfile()]
+        except FileNotFoundError:
+            print(f"Pickled data root {pick_root} not found.")
+            total_files = None
+
+    else:
+        total_files = None
+
+    total_files = comm.bcast(total_files, root=0)
+
+    # Pickle root was not found
+    if total_files is None:
+        raise SystemExit(0)
+
+    mp_con.set_full_list(total_files)
+    worker_list: list[str] = mp_con.generate_worker_list()
+
+    # Keep track of peak log for each alignment
+    peak_log_local = []
+
+    # Iterate through alignment datafiles -------------------------------------
+
+    out_dir = Path(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    # First pass; find suitable candidates if present
+    for pick_file in worker_list:
+        # Load in the alignment data object from the archive
+        segmented = _load_alignment_data(pick_root, pick_file, rank)
+        if segmented is None:
+            continue
+
+        alignDat = segmented.total
+
+        switch_size = alignDat.switch_end - alignDat.switch_start
+
+        # Save path formation and sample/ref IDs
+        ref = Path(pick_file[:-2]).name
+        targetname = Path(pick_file).parts[-3]
+        transcriptome = Path(pick_file).parts[-2]
+
+        # Check whether there are enough reads to proceed.
+        # This needs to be done to avoid clutter in the results
+        # that failed the filter
+        max_cov = max(alignDat.readcov[alignDat.switch_start : alignDat.switch_end])
+        roi = (checker.relative_size_bounds, checker.relative_size_bounds)
+
+        plotdec = "fail"
+
+        # Iterate through each peak in the region of interest -----------------
+        for peak in get_peaks(alignDat, ss_margins=ext_prop, reg_of_i=roi):
+            # One row per peak
+            peak_info = {}
+            peak_info["rowid"] = ref
+            peak_info["transcriptome"] = str(transcriptome)
+
+            if max_cov < min_cov_depth:
+                peak_info["decision_note"] = (
+                    f"Minimum coverage not reached ({max_cov} < {min_cov_depth})"
+                )
+                peak_log_local.append(peak_info)
+                break
+
+            if _process_peak(peak, peak_info, peak_log_local):
+                plotdec = "pass"
+
+        if max_cov < min_cov_depth:
+            continue
+
+        passfaildir = plotdec
+
+        # main plot path
+        save_path = out_dir.joinpath(passfaildir, f"{ref}#{transcriptome}.png")
+        save_path.parent.mkdir(exist_ok=True, parents=True)
+
+        # stat plot path
+        stat_path = out_dir.joinpath(passfaildir, f"{ref}#{transcriptome}_stats.png")
+
+        # Calculate and set info for binned raw ends
+        segmented.bin_rawends(bin_size=bin_size)
+
+        # We still want to see a large region around the switch in the plots
+        lplot = 1.0 if ext_prop[0] < 1.0 else ext_prop[0]
+        rplot = 1.0 if ext_prop[1] < 1.0 else ext_prop[1]
+
+        lbuff = int(switch_size * lplot)
+        rbuff = int(switch_size * rplot)
+        end_buffers = [lbuff, rbuff]
+
+        pObj = CoveragePlot(segmented, end_buffers)
+        if conv:
+            pObj._with_conv(save_path)
+
+        else:
+            pObj.default(save_path)
+
+        if statplot:
+            pObj.distribution_plots(stat_path)
+
+    peak_log_arr = comm.gather(peak_log_local, root=0)
+
+    if rank == 0:
+        if peak_log_arr is None:
+            raise TypeError("Gather failed")
+        log = []
+        for instance_arr in peak_log_arr:
+            log.extend(instance_arr)
+
+        # Make dataframe
+        # df = pd.DataFrame({"target_name": classes, "pass_rate": rates})
+        # df.to_csv(f"{out_dir}/pass_rates.csv", index=False)
+        df = pd.DataFrame(log)
+        df.to_csv(f"{out_dir}/peak_log.csv", index=False)
+
+
+def depr_main(
+    pick_root: str, out_dir, bin_size, min_cov_depth, ext_prop, conv, statplot
+):
     # Determine MPI context
     mp_con = BasicMPIContext()
     comm = mp_con.comm

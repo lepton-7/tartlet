1909ab4e566ddfb79ae9861a1e83fc10dfbf8e7b
lepton-7 <sachitkshatriya2609@gmail.com>
2024-02-08 01:50:11 -0500

    Added drop significance check

--- tartlet/utils/filter_functions.py
+++ tartlet/utils/filter_functions.py
@@ -5,27 +5,35 @@ class DefaultThresholds:
     relative_size_bounds = 0.3
     relative_cov_delta = -0.2
     symmetric_peaks_pval = 0.05
+    peak_sig_thresh = 0.05
 
     def __init__(self) -> None:
         pass
-    
+
     @staticmethod
     def check(cand: Candidate):
+        d = DefaultThresholds
         issues = []
         warns = []
 
-        if abs(cand.from_switch_end_relative) > DefaultThresholds.relative_size_bounds:
+        if cand.coverage_drop_pvalue >= d.peak_sig_thresh:
             issues.append(
-                f"Too far from end (abs({cand.from_switch_end_relative})>{DefaultThresholds.relative_size_bounds})"
+                f"Drop not significant ({cand.coverage_drop_pvalue:.2f} >= {d.peak_sig_thresh})"
             )
-        if cand.rel_cov_delta > DefaultThresholds.relative_cov_delta:
+
+        if abs(cand.from_switch_end_relative) > d.relative_size_bounds:
             issues.append(
-                f"Small drop ({cand.rel_cov_delta}>{DefaultThresholds.relative_cov_delta})"
+                f"Too far from end (abs({cand.from_switch_end_relative:.2f}) > {d.relative_size_bounds})"
             )
-        if cand.symks_pval > DefaultThresholds.symmetric_peaks_pval:
-            warns.append(
-                f"Symmetry check failed (>{DefaultThresholds.symmetric_peaks_pval})"
+        if cand.rel_cov_delta > d.relative_cov_delta:
+            issues.append(
+                f"Small drop ({cand.rel_cov_delta:.2f} > {d.relative_cov_delta})"
             )
+
+        # if cand.symks_pval > d.symmetric_peaks_pval:
+        #     warns.append(
+        #         f"Symmetry check failed (>{d.symmetric_peaks_pval})"
+        #     )
 
         if len(issues) > 0:
             cand.note = ";".join(issues)
